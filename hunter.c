46f6e6286b45b4f3154c385a775404a32740f29f
Eric Bai <baixu.must@gmail.com>
2020-08-03 18:48:29 +1000

    random move version for hunter

--- hunter.c
+++ hunter.c
@@ -54,119 +54,123 @@ void decideHunterMove(HunterView hv)
     return;
     }
 
-	PlaceId locat = NOWHERE;
-	int currPlayer = HvGetPlayer(hv);
+	// PlaceId locat = NOWHERE;
+	// int currPlayer = HvGetPlayer(hv);
 	
-	// If hunter's blood is less than a perticular value, hunter rests.
-	if (HvGetHealth(hv,HvGetPlayer(hv)) <= 4) {
-		int nextLoc = HvGetPlayerLocation(hv,HvGetPlayer(hv));
-		registerBestPlay((char *)placeIdToAbbrev(nextLoc), "Hunter rest");
-		return;
-	} 
+	// // If hunter's blood is less than a perticular value, hunter rests.
+	// if (HvGetHealth(hv,HvGetPlayer(hv)) <= 4) {
+	// 	int nextLoc = HvGetPlayerLocation(hv,HvGetPlayer(hv));
+	// 	registerBestPlay((char *)placeIdToAbbrev(nextLoc), "Hunter rest");
+	// 	return;
+	// } 
 
-	// Get the trail of dracula: We have to analyze the informations in the trail
-	int *trail = HvReturnTrail(hv);
+	// // Get the trail of dracula: We have to analyze the informations in the trail
+	// int *trail = HvReturnTrail(hv);
 	
-	// Trace vampire firstly in the early stage of game if any location of it revealed
-	// If any hunter can get to the location of vampire before it become mature,
-	// kill unmature vampire first 
-	int vampLoc = HvGetVampireLocation(hv);
-	if (vampLoc < 71 &&  vampLoc > -1 && HvGetScore(hv) > 300) {// We define "early stage of game" when game score
-													// not lower than 300
-		// Calculate how many turns it will mature
-		int matureRound = -1;
-		int distance;		
-		for (int i = 0; i < 6; i++) {
-			if (trail[i] == NOWHERE) 
-				matureRound = 5 - i;
-		}
-		assert(matureRound != -1);
+	// // Trace vampire firstly in the early stage of game if any location of it revealed
+	// // If any hunter can get to the location of vampire before it become mature,
+	// // kill unmature vampire first 
+	// int vampLoc = HvGetVampireLocation(hv);
+	// if (vampLoc < 71 &&  vampLoc > -1 && HvGetScore(hv) > 300) {// We define "early stage of game" when game score
+	// 												// not lower than 300
+	// 	// Calculate how many turns it will mature
+	// 	int matureRound = -1;
+	// 	int distance;		
+	// 	for (int i = 0; i < 6; i++) {
+	// 		if (trail[i] == NOWHERE) 
+	// 			matureRound = 5 - i;
+	// 	}
+	// 	assert(matureRound != -1);
 
-		// Eastimate whether current hunter can get there
-		HvGetShortestPathTo(hv,HvGetPlayer(hv), vampLoc, &distance);
-		if (distance <= matureRound) {
-			int nextMove = returnNext(vampLoc, currPlayer, hv);
-			if (farEnough(hv, nextMove)) 
-				registerBestPlay((char *)placeIdToAbbrev(nextMove), "Have we nothing Toulouse?");
-			else
-				registerBestPlay((char *)placeIdToAbbrev(randomMove(hv)), "Have we nothing Toulouse?");
-			return;
-		}
-	}
+	// 	// Eastimate whether current hunter can get there
+	// 	HvGetShortestPathTo(hv,HvGetPlayer(hv), vampLoc, &distance);
+	// 	if (distance <= matureRound) {
+	// 		int nextMove = returnNext(vampLoc, currPlayer, hv);
+	// 		if (farEnough(hv, nextMove)) 
+	// 			registerBestPlay((char *)placeIdToAbbrev(nextMove), "Have we nothing Toulouse?");
+	// 		else
+	// 			registerBestPlay((char *)placeIdToAbbrev(randomMove(hv)), "Have we nothing Toulouse?");
+	// 		return;
+	// 	}
+	// }
 	
 
-	// Check if there are useful informations available
-	// If we can get informations about 
-	locat= validLastLocation(hv);
-	if(locat != NOWHERE) {
+	// // Check if there are useful informations available
+	// // If we can get informations about 
+	// locat= validLastLocation(hv);
+	// if(locat != NOWHERE) {
 
-		// Traveral the move history
-		for (int i = 0; i < TRAIL_SIZE; i++) {
+	// 	// Traveral the move history
+	// 	for (int i = 0; i < TRAIL_SIZE; i++) {
 
-			// If we found doubleback move
-			if (trail[i] <= DOUBLE_BACK_5 && trail[i] >= DOUBLE_BACK_1) {
+	// 		// If we found doubleback move
+	// 		if (trail[i] <= DOUBLE_BACK_5 && trail[i] >= DOUBLE_BACK_1) {
 
-				// We find whether the location doubleback pointed has been revealed
-				int backMove = trail[i] - DOUBLE_BACK_1 + 1;
+	// 			// We find whether the location doubleback pointed has been revealed
+	// 			int backMove = trail[i] - DOUBLE_BACK_1 + 1;
 
-				assert(i - backMove < 0);
-				// Previous location has been revealed
-				if (trail[i - backMove] < 71 &&
-				trail[i - backMove] > -1 ) {
-					int nextMove = returnNext(trail[i - backMove], currPlayer, hv);
+	// 			assert(i - backMove < 0);
+	// 			// Previous location has been revealed
+	// 			if (trail[i - backMove] < 71 &&
+	// 			trail[i - backMove] > -1 ) {
+	// 				int nextMove = returnNext(trail[i - backMove], currPlayer, hv);
 					
-					// Eastimate which mode to use
-					if (moveMode(hv, trail[i - backMove]) == RANDOM)
-						registerBestPlay((char *)placeIdToAbbrev(randomMove(hv)), "Catch ya!");	
-					else if (moveMode(hv, trail[i - backMove]) == ON_PURPOSE)
-						registerBestPlay((char *)placeIdToAbbrev(nextMove), "Catch ya!");	
+	// 				// Eastimate which mode to use
+	// 				if (moveMode(hv, trail[i - backMove]) == RANDOM)
+	// 					registerBestPlay((char *)placeIdToAbbrev(randomMove(hv)), "Catch ya!");	
+	// 				else if (moveMode(hv, trail[i - backMove]) == ON_PURPOSE)
+	// 					registerBestPlay((char *)placeIdToAbbrev(nextMove), "Catch ya!");	
 
-					return;
-				} else {
-					continue;
-				}
-			}
+	// 				return;
+	// 			} else {
+	// 				continue;
+	// 			}
+	// 		}
 			
-			// We found real location
-			if (trail[i] < 71 && trail[i] > -1) {
-				int nextMove = returnNext(trail[i], currPlayer, hv);
-				// estimate which mode to use
-				if (moveMode(hv, trail[i]) == RANDOM)
-					registerBestPlay((char *)placeIdToAbbrev(randomMove(hv)), "Catch ya!");	
-				else if (moveMode(hv, trail[i]) == ON_PURPOSE)
-					registerBestPlay((char *)placeIdToAbbrev(nextMove), "Catch ya!");
+	// 		// We found real location
+	// 		if (trail[i] < 71 && trail[i] > -1) {
+	// 			int nextMove = returnNext(trail[i], currPlayer, hv);
+	// 			// estimate which mode to use
+	// 			if (moveMode(hv, trail[i]) == RANDOM)
+	// 				registerBestPlay((char *)placeIdToAbbrev(randomMove(hv)), "Catch ya!");	
+	// 			else if (moveMode(hv, trail[i]) == ON_PURPOSE)
+	// 				registerBestPlay((char *)placeIdToAbbrev(nextMove), "Catch ya!");
 				
-				return;
-			}
-		}
-	}
+	// 			return;
+	// 		}
+	// 	}
+	// }
 	
-	// We can't find any useful information in the trail
+	// // We can't find any useful information in the trail
 	
-	// If we are in the first 6 rounds, do random moves
-	if (HvGetRound(hv) < 6) {
-		registerBestPlay((char *)placeIdToAbbrev(randomMove(hv)), "Playing around here");
-		return;
-	}
+	// // If we are in the first 6 rounds, do random moves
+	// if (HvGetRound(hv) < 6) {
+	// 	registerBestPlay((char *)placeIdToAbbrev(randomMove(hv)), "Playing around here");
+	// 	return;
+	// }
 	
-	// Eastimate if hunters have researched withing 3 turns
-	// If we have done research withing 3 rounds, we choose one move mode 
-	// tring to find out the location of dracula
-	int draculaLocRound;
-	PlaceId draculaLastLoc= HvGetLastKnownDraculaLocation(hv, &draculaLocRound);
-	if (HvGetRound(hv) - draculaLocRound - TRAIL_SIZE <= 3) {
+	// // Eastimate if hunters have researched withing 3 turns
+	// // If we have done research withing 3 rounds, we choose one move mode 
+	// // tring to find out the location of dracula
+	// int draculaLocRound;
+	// PlaceId draculaLastLoc= HvGetLastKnownDraculaLocation(hv, &draculaLocRound);
+	// if (HvGetRound(hv) - draculaLocRound - TRAIL_SIZE <= 3) {
 		
-		MoveMode mode = moveMode(hv, draculaLastLoc);
-		if (mode == RANDOM)
-			registerBestPlay((char *)placeIdToAbbrev(randomMove(hv)), "Playing around here");	
-		else if(mode == ON_PURPOSE)
-			registerBestPlay((char *)placeIdToAbbrev(draculaLocRound), "Go find dracula's place");
+	// 	MoveMode mode = moveMode(hv, draculaLastLoc);
+	// 	if (mode == RANDOM)
+	// 		registerBestPlay((char *)placeIdToAbbrev(randomMove(hv)), "Playing around here");	
+	// 	else if(mode == ON_PURPOSE)
+	// 		registerBestPlay((char *)placeIdToAbbrev(draculaLocRound), "Go find dracula's place");
 
 
-	} else { // We haven't done researches withing tree rounds, research!
-		int nextMove = HvGetPlayerLocation(hv, HvGetPlayer(hv));
-		registerBestPlay((char *)placeIdToAbbrev(nextMove), "Do some research");
-	}
+	// } else { // We haven't done researches withing tree rounds, research!
+	// 	int nextMove = HvGetPlayerLocation(hv, HvGetPlayer(hv));
+	// 	registerBestPlay((char *)placeIdToAbbrev(nextMove), "Do some research");
+	// }
+
+
+	// Randomly move
+	registerBestPlay((char *)placeIdToAbbrev(randomMove(hv)), "Playing around here");
 	return;
 }
 
